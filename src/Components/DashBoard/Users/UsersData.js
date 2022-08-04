0435ad65ce4b246d7be233e47a06e6530d76d46d
Nizampatnamsai <jesusjoshuajesus@gmail.com>
2022-08-04 12:38:57 +0530

    users data responsive

--- src/Components/DashBoard/Users/UsersData.js
+++ src/Components/DashBoard/Users/UsersData.js
@@ -11,9 +11,13 @@ const UsersData = ({id,name,table,active,email,index,survedby}) => {
 
         <table className='UserData_table'>
             <tr>
-                <td className='UserData_name'>{name}</td>
-                <td className='UserData_tablenumb'>{table?table:'not yet' }</td>
-                <td className='UserData_Active'>{active ? <button>Yes</button>:'-'}</td>
+              
+            <td className='UserData_name'>{name}</td>
+              <td className='UserData_tablenumb'>{table?table:'not yet' }</td>
+                
+              <td className='UserData_Active'>{active ? <button>Yes</button>:'-'}</td>
+
+                
                 <td className='UserData_Survedby'>{survedby ?
                 <div>
                   <span className='UserData_Survedby_name'>{survedname[0]}</span>
@@ -29,6 +33,52 @@ const UsersData = ({id,name,table,active,email,index,survedby}) => {
                 </tr>
         </table>
 
+
+   <div className='UserData_forphones'>
+        <table className=''>
+          <tr>
+            <div>
+              <td>
+                Name
+              </td>
+            <td className='UserData_name'>{name}</td>
+
+            </div>
+            <div>
+              <td>Table booked</td>
+              <td className='UserData_tablenumb'>{table?table:'not yet' }</td>
+
+            </div>
+
+            <div>
+              <td>active</td>
+              <td className='UserData_Active'>{active ? <button>Yes</button>:'-'}</td>
+
+            </div>
+
+            <div>
+              <td>Survedby</td>
+              <td className='UserData_Survedby'>{survedby ?
+                <div>
+                  <span className='UserData_Survedby_name'>{survedname[0]}</span>
+
+                  <span className='UserData_Survedby_email'>{survedname}</span>
+                  
+                </div>
+                
+                
+                :'Yet to be decided by you'}</td>
+            </div>
+
+            <div>
+              <td>email</td>
+              <td className='UserData_email'>{email}</td>
+
+            </div>
+          </tr>
+
+        </table>
+        </div>
         </div>
       
     </div>
