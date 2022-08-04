8f8bd177928b54f264c46cdec96f12aed7c07d10
Nizampatnamsai <jesusjoshuajesus@gmail.com>
2022-08-04 13:12:18 +0530

    supliers data responsive

--- src/Components/DashBoard/Supplier/SuppliersData.js
+++ src/Components/DashBoard/Supplier/SuppliersData.js
@@ -6,9 +6,10 @@ const SuppliersData = ({id,index,name,email,password,active,survingTable}) => {
        <table className='SuppliersData'>
   
   <tr>
+    
      <td className='Supliers_name'>{name} </td>
      <td className='Supliers_Survedby'>{email}</td>
-     {/* <td className='Supliers_Survedby'>{password}</td> */}
+    
      <td className='Supliers_Active'>
          {active ? <button className='Tabledata_btn_booked'>Active</button>:
           <button className='Tabledata_btn_notbooked'>not Active</button>}
@@ -20,6 +21,44 @@ const SuppliersData = ({id,index,name,email,password,active,survingTable}) => {
  
 
 </table>
+
+<div className='Supliersdata_phones'>
+
+<table>
+  <tr>
+   <div>
+    <td>Name</td>
+    <td className='Supliers_name'>{name} </td>
+
+   </div>
+  
+
+
+  <div>
+    <td>email</td>
+    <td className='Supliers_Survedby'>{email}</td>
+
+  </div>
+
+  <div>
+    <td>active</td>
+    <td className='Supliers_Active'>
+         {active ? <button className='Tabledata_btn_booked'>Active</button>:
+          <button className='Tabledata_btn_notbooked'>not Active</button>}
+         </td>
+  </div>
+
+  <div>
+    <td>
+      survingTable
+    </td>
+    <td className='Supliers_tablenumb'> {survingTable? survingTable: '--'}</td>
+
+  </div>
+  </tr>
+</table>
+
+</div>
     </div>
   )
 }
